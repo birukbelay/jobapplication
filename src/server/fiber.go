56facd94f35ede077e9e13e9cee8f67082120d07
biruk <biruk>
2025-08-09 12:54:57 +0300

    fixed job queries

--- src/server/fiber.go
+++ src/server/fiber.go
@@ -7,6 +7,8 @@ import (
 	"github.com/projTemplate/goauth/src/app/account/profile"
 	admins "github.com/projTemplate/goauth/src/app/admin/admin_users"
 	"github.com/projTemplate/goauth/src/app/general/upload"
+	"github.com/projTemplate/goauth/src/app/job/applications"
+	"github.com/projTemplate/goauth/src/app/job/jobs"
 	"github.com/projTemplate/goauth/src/models"
 	conf "github.com/projTemplate/goauth/src/models/config"
 	"github.com/projTemplate/goauth/src/providers"
@@ -28,7 +30,19 @@ func SetHumaCoreRoutes(humaRouter huma.API, dbs *providers.IProviderS, conf *con
 	//company owners management routes
 
 	upload.SetupUploadRoutes(humaRouter, dbs, upload.NewUploadGormServ(dbs))
+	SetupJobModule(humaRouter, dbs)
 
 	// //common routes
 	// upload2.SetupUploadRoutes(humaRouter, cmnService, upload2.NewUploadGormServ(dbs.Gorm, cmnService))
+}
+
+// SetupJobModule initializes all job-related routes
+func SetupJobModule(humaRouter huma.API, cmnServ *providers.IProviderS) {
+	// Initialize services
+	jobService := jobs.NewService(cmnServ)
+	applicationService := applications.NewService(cmnServ)
+
+	// Setup routes
+	jobs.SetupJobRoutes(humaRouter, cmnServ, jobService)
+	applications.SetupApplicationRoutes(humaRouter, cmnServ, applicationService)
 }
