148a3e8550fb8a86e5d1221f8f11fc35a46b19cd
biruk <biruk>
2025-08-09 10:40:02 +0300

    removed the server and moved it tocmn

--- main.go
+++ main.go
@@ -1,39 +1,29 @@
 package main
 
 import (
-	"embed"
-	"io/fs"
-
 	cmnConf "github.com/birukbelay/gocmn/src/config"
 	"github.com/birukbelay/gocmn/src/provider/db/redis"
 	sql_db "github.com/birukbelay/gocmn/src/provider/db/sql"
 	email "github.com/birukbelay/gocmn/src/provider/email/smtp"
 	"github.com/birukbelay/gocmn/src/provider/upload/cloudinaryServ"
+	"github.com/birukbelay/gocmn/src/server"
 
 	"github.com/projTemplate/goauth/public/static/templates"
 	"github.com/projTemplate/goauth/src/models/config"
 	"github.com/projTemplate/goauth/src/models/migration"
 	Idb "github.com/projTemplate/goauth/src/providers"
-	"github.com/projTemplate/goauth/src/server"
+	locSrv "github.com/projTemplate/goauth/src/server"
 )
 
-//go:embed public/static/styles
-var EmbedAsset embed.FS
-
 func main() {
-	// Embedding the huma documentation assets
-	embeddedAssets, err := fs.Sub(EmbedAsset, "public/static/styles")
-	if err != nil {
-		panic(err)
-	}
-	server.EmbeddedAssets = embeddedAssets
+
 	//Loading the config
 	conf := cmnConf.LoadConfigT[config.EnvConfig]()
 	//Creating the database connection
 	Db, _ := sql_db.NewSqlDb(&conf.SqlDbConfig)
 	migration.MigrateDb2(Db)
 	//Creating redis service
-	redis, err := redis.NewRedis(&conf.KeyValConfig)
+	redis, _ := redis.NewRedis(&conf.KeyValConfig)
 	//email sending provider, also serves as verification code sender
 	emailSender := email.NewSmtp(conf.SmtpHost, conf.SmtpPort, conf.SmtpPwd, conf.SmtpUsername, templates.Embedded)
 	//Creating upload service
@@ -43,6 +33,6 @@ func main() {
 	//create the server with default configs
 	srvr := server.CreateFiber(conf.ServerHost, conf.ServerPort)
 	//Setting up the routes
-	server.SetHumaCoreRoutes(srvr.HumaRouter, provider, conf)
+	locSrv.SetHumaCoreRoutes(srvr.HumaRouter, provider, conf)
 	_ = srvr.Listen()
 }
--- src/server/fiber.go
+++ src/server/fiber.go
@@ -1,16 +1,7 @@
 package server
 
 import (
-	"fmt"
-	"net/http"
-
-	cmn "github.com/birukbelay/gocmn/src/logger"
 	"github.com/danielgtaylor/huma/v2"
-	"github.com/danielgtaylor/huma/v2/adapters/humafiber"
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/filesystem"
-	zlog "github.com/rs/zerolog/log"
 
 	"github.com/projTemplate/goauth/src/app/account/auth"
 	"github.com/projTemplate/goauth/src/app/account/profile"
@@ -24,63 +15,6 @@ import (
 	conf "github.com/projTemplate/goauth/src/models/config"
 	"github.com/projTemplate/goauth/src/providers"
 )
-
-type FiberServer struct {
-	Engine     *fiber.App
-	HumaRouter huma.API
-	ServerHost string
-	ServerPort string
-}
-
-func CreateFiber(host, port string) *FiberServer {
-
-	app := fiber.New()
-	app.Use(cors.New())
-
-	app.Get("/fiber", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
-	app.Get("/docs", ServFiberDoc)
-
-	//huma related path
-	config := huma.DefaultConfig("", "")
-	config.DefaultFormat = "application/json"
-	config.DocsPath = "/"
-	serv := &FiberServer{
-		Engine:     app,
-		ServerHost: host,
-		ServerPort: port,
-	}
-	serv.SetupMiddleware()
-
-	humaRouter := humafiber.NewWithGroup(app, app, config)
-	serv.HumaRouter = humaRouter
-
-	return serv
-}
-func (s *FiberServer) SetupMiddleware() {
-	// s.Engine.Use(gin.Logger())
-	// s.Engine.Use(gin.Recovery())
-	s.Engine.Static("/assets", "./public/assets")
-	s.Engine.Use("/static", filesystem.New(filesystem.Config{
-		Root: http.FS(EmbeddedAssets),
-		// PathPrefix: "static",
-	}))
-}
-
-func (s *FiberServer) Listen() error {
-
-	s.Engine.Get("/ping", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
-	cmn.LogTrace("server started at", fmt.Sprintf("http://127.0.0.1:%s/docs", s.ServerPort))
-	err := s.Engine.Listen(s.ServerHost + ":" + s.ServerPort)
-
-	if err != nil {
-		zlog.Panic().Err(err).Msg("listen Error")
-	}
-	return err
-}
 
 func SetHumaCoreRoutes(humaRouter huma.API, dbs *providers.IProviderS, conf *conf.EnvConfig) {
 
